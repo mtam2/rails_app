3fa700e2c6f4e529a4a04596500a092a38ecb3b9
mtam2 <maximtea.cloud@gmail.com>
2015-12-18 16:51:11 +0000

    Added follow user implementation

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,17 @@ Rails.application.routes.draw do
   get 'signup'    => 'users#new'
   get 'login'     => 'sessions#new'
   post 'login'    => 'sessions#create'
+  
   delete 'logout' => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
 
 # Rails.application.routes.draw do
